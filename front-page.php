80dc5f91e35ae948725443990a33d60567bdb03c
michaelgombos <gombos@2002@gmail.com>
2022-08-18 22:24:49 +0000

    created header.php and footer.php, added added frontpage php

--- front-page.php
+++ front-page.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="utf-8">
-      <title>EVER SKETCH STATIC HTML BUILD</title>
-      <meta name="author" content="Michael Gombos">
-      <meta name="description" content="Static html site for future wordpress theme">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="style.css" rel="stylesheet">
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-   
+<?php
+   get_header();
+?>
       
-   
-   </head>
-   <body>
-      <header>
-         <div class="links">
-            <a href="#" > <img class="logo" src="./assets/template-logo/logo-primary.svg"></a>
-            <nav>
-               <a class="link" href="#" >All</a>
-               <a class="link" href="#" >Art</a>
-               <a class="link" href="#" >Programming</a>
-               <a class="link" href="#" >Book Reviews</a>
-            </nav>
-         </div>
-      </header>
       <div class="page-wrap">
          <main>
             <section id="hero">
@@ -177,6 +153,7 @@
             </nav>
          </div>
       </footer>
-      <script src="magic.js"></script>
-   </body>
-</html>
+      
+<?php
+get_footer();
+?>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>EVER SKETCH STATIC HTML BUILD</title>
+      <meta name="author" content="Michael Gombos">
+      <meta name="description" content="Static html site for future wordpress theme">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <?php
+     wp_head();
+     ?>
+   </head>
+   <body>
+      <header>
+         <div class="links">
+            <a href="#" > <img class="logo" src="./assets/template-logo/logo-primary.svg"></a>
+            <nav>
+               <a class="link" href="#" >All</a>
+               <a class="link" href="#" >Art</a>
+               <a class="link" href="#" >Programming</a>
+               <a class="link" href="#" >Book Reviews</a>
+            </nav>
+         </div>
+      </header>
