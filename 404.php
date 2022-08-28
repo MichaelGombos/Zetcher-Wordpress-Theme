8f94c1cd9c6e9704e8586dffa6e8a2266c8f4834
michaelgombos <gombos@2002@gmail.com>
2022-08-28 16:48:46 +0000

    added search and 404 pages

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+   get_header();
+?>
+      
+
+            <section class="page-content">
+                <h1>Page not found</h1>
+                
+                <?php
+                    get_search_form();
+                ?>
+            </section>
+         </main>
+
+      
+<?php
+get_footer();
+?>
